984b4771b7680e48b181e74f96a26ce7c80d37d4
garrettwilloughby <garrettwilloughby@gmail.com>
2023-07-09 22:29:25 -0400

    Update

--- script.js
+++ script.js
@@ -1,104 +1,50 @@
 
-const sqlite3 = require('sqlite3').verbose(); //require extension
+import initSqlJs from 'sql.js';
 
-let sql;
-
-//connect to database
-const db = new sqlite3.Database('./data.db', sqlite3.OPEN_READWRITE, (err) =>{
-  if (err) return console.error(err.message);
-})
-
-//create table
-const create = () => {
-sql = 'CREATE TABLE terms(id INTEGER PRIMARY KEY, ENGLISH, CHINESE)';
-db.run(sql);
-}
-
-//drop table
-const drop = () => {
-db.run("DROP TABLE terms");
-}
-
-//Insert data
-const insert = (english, chinese) =>{
-sql = 'INSERT INTO terms(ENGLISH, CHINESE) VALUES (?, ?)';
-  db.run(sql, [english, chinese], (err) => {
-    if (err) return console.error(err.message);
-  })
-}
-
-
-//Query Data
-const query = () => {
-sql = 'SELECT * FROM terms';
-db.all(sql, [], (err, rows) => {
-  if (err) return console.error(err.message);
-  rows.forEach((row) => {
-    console.log(row);
-  });
+const SQL = await initSqlJs({
+  // Required to load the wasm binary asynchronously. Of course, you can host it wherever you want
+  // You can omit locateFile completely when running in node
+  locateFile: file => `https://sql.js.org/dist/${file}`
 });
-}
+
+const db = new SQL.Database();
+
+const createDatabase = () => {
+  let sqlstr = 'CREATE TABLE terms(id INTEGER PRIMARY KEY, ENGLISH, CHINESE)';
+  db.run(sqlstr);
+};
+
+createDatabase();
+
+const front = document.querySelectorAll('.front')[0]; //need indexes because style for querySelectorAll is a list
+const back = document.querySelectorAll('.back')[0];
+const chinese = document.querySelectorAll('.chinese');
+const english = document.querySelectorAll('.english');
 
 
-//delete data
+const flip = () => {
+    if (front.style.display !== "none") {
+      front.style.display = "none";
+      back.style.display = "block";
+    } else {
+      front.style.display = "block";
+      back.style.display = "none";
+    }
+  }
 
-const deleted = (id) => {
-sql = 'DELETE FROM terms WHERE id = ?';
-db.run(sql, [id], (err) => {
-  if (err) return console.error(err.message);
-});
-}
+  words = {
+    Hello: "Ni hao",
+    Watermelon: "Xigua"
+  }
 
-
-
-//update data
-const update = () => {
-sql = 'UPDATE terms SET ENGLISH = ? CHINESE = ? WHERE id = ?';
-db.run(sql, [english, chinese, id], (err) => {
-  if (err) return console.error(err.message);
-});
-}
-
-
-
-
-// const front = document.querySelectorAll('.front')[0]; //need indexes because style for querySelectorAll is a list
-// const back = document.querySelectorAll('.back')[0];
-// const flips = document.querySelectorAll('.flip');
-// const chinese = document.querySelectorAll('.chinese');
-// const english = document.querySelectorAll('.english');
-
-
-
-
-// const flip = () => {
-//     if (front.style.display !== "none") {
-//       front.style.display = "none";
-//       back.style.display = "block";
-//     } else {
-//       front.style.display = "block";
-//       back.style.display = "none";
-//     }
-//   }
-
-//   words = {
-//     Hello: "Ni hao",
-//     Watermelon: "Xigua"
-    
-//   }
-
-//   data = Object.entries(words);
+  data = Object.entries(words);
   
-//   const getRandomTerm = () => {
-//     let randomTerm = data[Math.floor(Math.random() * data.length)]
-//     front.innerHTML = `<p>${randomTerm[0]}</p>`; //note the use of literals
-//     back.innerHTML =  `<p>${randomTerm[1]}</p>`; //replaces content that lives in p with a random term.
-//   }
+  const getRandomTerm = () => {
+    let randomTerm = data[Math.floor(Math.random() * data.length)]
+    front.innerHTML = `<p>${randomTerm[0]}</p>`; //note the use of literals
+    back.innerHTML =  `<p>${randomTerm[1]}</p>`; //replaces content that lives in p with a random term.
+  }
 
-//   const nextTerm = () => {
-//     getRandomTerm();
-//   }
-
-//   const addTerm = () => {
-//     words[english] = chinese
-//   }
+  const nextTerm = () => {
+    getRandomTerm();
+  }
