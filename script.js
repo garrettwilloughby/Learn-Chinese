a2b296638cdc9805319bc763241986c7b24cd6ec
garrettwilloughby <garrettwilloughby@gmail.com>
2023-07-11 19:15:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,50 @@
-const initSqlJs = require('sql.js');
-src='/dist/sql-wasm.js'
+//check if user's browser supports some instance of IndexedDB.
+const indexedDB = 
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
 
+//opens a TermsDatabase upon opening browser. Creates database if does not exist already. 
+const request = indexedDB.open("TermsDatabase", 1);
 
-config = {
-  locateFile: file => `/Users/garrettwilloughby/Desktop/Personal Projects/Learning Chinese/Learn-Chinese/sql-wasm.wasm`
+//if browser is unable to open databsae.
+request.onerror = function(event){
+  console.error("Error occured with IndexedDB");
+  console.error(event);
 };
 
-initSqlJs(config).then(function(SQL){
-  //Create the database
-  const db = new SQL.Database();
-  // Run a query without reading the results
-  db.run("CREATE TABLE test (col1, col2);");
-  // Insert two rows: (1,111) and (2,222)
-  db.run("INSERT INTO test VALUES (?,?), (?,?)", [1,111,2,222]);
 
-  // Prepare a statement
-  const stmt = db.prepare("SELECT * FROM test WHERE col1 BETWEEN $start AND $end");
-  stmt.getAsObject({$start:1, $end:1}); // {col1:1, col2:111}
-
-  // Bind new values
-  stmt.bind({$start:1, $end:2});
-  while(stmt.step()) { //
-    const row = stmt.getAsObject();
-    console.log('Here is a row: ' + JSON.stringify(row));
-}
-});
-
-  // const front = document.querySelectorAll('.front')[0]; //need indexes because style for querySelectorAll is a list
-  // const back = document.querySelectorAll('.back')[0];
-  // const chinese = document.querySelectorAll('.chinese');
-  // const english = document.querySelectorAll('.english');
+  const front = document.querySelectorAll('.front')[0]; //need indexes because style for querySelectorAll is a list
+  const back = document.querySelectorAll('.back')[0];
+  const chinese = document.querySelectorAll('.chinese');
+  const english = document.querySelectorAll('.english');
 
 
-  // const flip = () => {
-  //     if (front.style.display !== "none") {
-  //       front.style.display = "none";
-  //       back.style.display = "block";
-  //     } else {
-  //       front.style.display = "block";
-  //       back.style.display = "none";
-  //     }
-  //   }
+  const flip = () => {
+      if (front.style.display !== "none") {
+        front.style.display = "none";
+        back.style.display = "block";
+      } else {
+        front.style.display = "block";
+        back.style.display = "none";
+      }
+    }
 
-  //   words = {
-  //     Hello: "Ni hao",
-  //     Watermelon: "Xigua"
-  //   }
+    words = {
+      Hello: "Ni hao",
+      Watermelon: "Xigua"
+    }
 
-  //   data = Object.entries(words);
+    data = Object.entries(words);
     
-  //   const getRandomTerm = () => {
-  //     let randomTerm = data[Math.floor(Math.random() * data.length)]
-  //     front.innerHTML = `<p>${randomTerm[0]}</p>`; //note the use of literals
-  //     back.innerHTML =  `<p>${randomTerm[1]}</p>`; //replaces content that lives in p with a random term.
-  //   }
+    const getRandomTerm = () => {
+      let randomTerm = data[Math.floor(Math.random() * data.length)]
+      front.innerHTML = `<p>${randomTerm[0]}</p>`; //note the use of literals
+      back.innerHTML =  `<p>${randomTerm[1]}</p>`; //replaces content that lives in p with a random term.
+    }
 
-  //   const nextTerm = () => {
-  //     getRandomTerm();
-  //   }
+    const nextTerm = () => {
+      getRandomTerm();
+    }
